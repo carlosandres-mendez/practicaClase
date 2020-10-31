aaa75acea791602688f690810145118095ef9a50
cmendez <carlosandresucr@gmail.com>
2020-10-30 21:35:57 -0600

    Se agregar ejemplo de String y switch

--- src/main/java/EjemploArray.java
+++ src/main/java/EjemploArray.java
@@ -14,6 +14,30 @@ public class EjemploArray {
     public static void main(String[] args){
         String[] miArreglo = {"a","y","r","t"};
         
-        System.out.println(miArreglo[2]);
+        String texto = "3+4";
+        String num1 = texto.substring(0, 1);
+        String oper = texto.substring(1, 2);
+        String num2 = texto.substring(2, 3);
+        
+        System.out.println("Num1: "+num1);
+        System.out.println("Oper: "+oper);
+        System.out.println("Num2: "+num2);
+        
+        int numero1 = Integer.parseInt(num1);  
+        int numero2 = Integer.parseInt(num2); 
+        
+        int resultado=0;
+        switch(oper){
+            case "+":{
+                resultado = numero1 + numero2;
+                break;
+            }
+            case "-":{
+                resultado = numero1 - numero2;
+                break; 
+            }   
+        }
+        
+        System.out.println("Resultado: "+resultado);
     }
 }
