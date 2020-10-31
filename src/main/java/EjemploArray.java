5b82aae077400c7d0b68f82cc1af8149527c0838
cmendez <carlosandresucr@gmail.com>
2020-10-30 20:58:15 -0600

    Se agrega ejemplo de arreglo

---
+++ src/main/java/EjemploArray.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java;
+
+/**
+ *
+ * @author carlosandres.mendez
+ */
+public class EjemploArray {
+    
+    public static void main(String[] args){
+        String[] miArreglo = {"a","y","r","t"};
+        
+        System.out.println(miArreglo[2]);
+    }
+}
